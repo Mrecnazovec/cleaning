5484ed44f4cd50226c50cb035834d329c0612d41
Mrecnazovec <101330874+Mrecnazovec@users.noreply.github.com>
2023-08-29 22:34:51 +0500

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,4 @@
-window.onload = function () {
-	document.body.classList.add('loaded_hiding');
-	window.setTimeout(function () {
-		document.body.classList.add('loaded');
-		document.body.classList.remove('loaded_hiding');
-	}, 500);
-}
+$(window).load(function () {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(100).fadeOut("slow");
+});
